ffd159f3a433788a5f93407e55846d81ede3388c
Victoria <bukachviko@gmail.com>
2023-08-22 13:26:46 +0300

    doing 6 section: fixed carousel

--- script/main.js
+++ script/main.js
@@ -1,40 +1,14 @@
-// Open the lightbox
-function openlightbox() {
-    document.getElementById('lightboxTeam').style.display = "block";
+const carousel = document.querySelector('.carousel__team');
+
+let isDrragging = false;
+
+const dragStart = () => {
+    isDrragging = true;
+}
+const dragging  = (e) => {
+    if(!isDrragging) return;
+    carousel.scrollLeft = e.pageX;
 }
 
-// Close the lightbox
-function closelightbox() {
-    document.getElementById('lightboxTeam').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides__team-big-avatar");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-}
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
