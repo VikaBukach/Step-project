af99eccc1a3cd40093176e2cecf24d1725ceb6af
Victoria <bukachviko@gmail.com>
2023-08-23 18:13:31 +0300

    added switch on mouse click for section 3 "Our Services"

--- script/main.js
+++ script/main.js
@@ -1,3 +1,45 @@
+'use strict'
+// --------------------------------3 секція Our Services--------------------
+const menuTabs = document.querySelectorAll('.ourServices__title');
+menuTabs.forEach((element) => {
+    element.addEventListener("click", (e) => {
+        const allTabs = document.querySelectorAll('.ourServices__title');
+        allTabs.forEach((element) => {
+            element.classList.remove('active');
+        })
+        e.target.classList.add('active');
+
+        const tabsContent = document.querySelectorAll('.ourServices__tabs-content li');
+        tabsContent.forEach((element) => {
+            element.style.display = 'none';
+        })
+        const dataAttribute = e.target.getAttribute('data-tab');
+        const foundEl = document.querySelector(`.ourServices__tabs-content [data-tab="${dataAttribute}"]`);
+        foundEl.style.display = 'flex';
+
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------------8 секція----Gallery of best images-----------------------------
 $(document).ready(function(){
     $('#gallery__container').masonry({
 // указываем элемент-контейнер в котором расположены блоки для динамической верстки
