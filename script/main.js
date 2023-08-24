1311681cafdc8fcad08fa8829ca972310cbb5052
Victoria <bukachviko@gmail.com>
2023-08-24 18:43:14 +0300

    added switch on mouse click for section 5 "Our amazing work"

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 'use strict'
 // --------------------------------3 секція Our Services--------------------
 const menuTabs = document.querySelectorAll('.ourServices__title');
+
 menuTabs.forEach((element) => {
     element.addEventListener("click", (e) => {
         const allTabs = document.querySelectorAll('.ourServices__title');
@@ -11,36 +12,98 @@ menuTabs.forEach((element) => {
 
         const tabsContent = document.querySelectorAll('.ourServices__tabs-content li');
         tabsContent.forEach((element) => {
-            element.style.display = 'none';
+            element.classList.remove('active');
         })
         const dataAttribute = e.target.getAttribute('data-tab');
         const foundEl = document.querySelector(`.ourServices__tabs-content [data-tab="${dataAttribute}"]`);
-        foundEl.style.display = 'flex';
+        foundEl.classList.add('active');
+    })
+})
+
+// -----------------5 секція------------------
+const amazingWorksTitles = document.querySelectorAll('.amazingWork__title');
+const showCards = 12;
+let currentCountCategoryItems = showCards;
+
+amazingWorksTitles.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+        amazingWorksTitles.forEach((tab) => {
+            tab.classList.remove('active');
+        })
+
+        e.target.classList.add('active');
+        const flippingCards = document.querySelectorAll('.flipping__card');
+        flippingCards.forEach((card) => {
+            card.classList.remove('active');
+        })
+
+
+        const dataCardAttribute = e.target.getAttribute('data-tab');
+        currentCountCategoryItems = showCards;
+        renderAmazingWorkCards(dataCardAttribute);
 
     })
-
 })
 
 
+function renderAmazingWorkCards(dataTab) {
+    let foundFlippingCards;
+    if (dataTab === 'cardAll') {
+        foundFlippingCards = document.querySelectorAll('.flipping__card')
+    } else {
+        foundFlippingCards = document.querySelectorAll(`.flipping__card[data-tab="${dataTab}"]`)
+    }
+
+    foundFlippingCards.forEach((card, index) => {
+        if (index < currentCountCategoryItems) {
+            card.classList.add('active');
+        }else {
+            card.classList.remove('active');
+        }
+    })
+
+    showAmazingWorkButton(dataTab)
+}
+
+const activeDataTab = document.querySelector('.amazingWork__title.active').getAttribute('data-tab')
+renderAmazingWorkCards(activeDataTab);
+showAmazingWorkButton(activeDataTab);
+
+function showAmazingWorkButton(dataTab) {
+    const btnLoadMore = document.querySelector('.amazingWork__btn');
+    let foundFlippingCards;
+    if (dataTab === 'cardAll') {
+        foundFlippingCards = document.querySelectorAll('.flipping__card')
+    } else {
+        foundFlippingCards = document.querySelectorAll(`.flipping__card[data-tab="${dataTab}"]`)
+    }
 
 
+    let foundFlippingActiveCards;
+    if (dataTab === 'cardAll') {
+        foundFlippingActiveCards = document.querySelectorAll('.flipping__card.active')
+    } else {
+        foundFlippingActiveCards = document.querySelectorAll(`.flipping__card.active[data-tab="${dataTab}"]`)
+    }
 
+    if (foundFlippingActiveCards.length < foundFlippingCards.length) {
+        btnLoadMore.style.display = 'flex';
+    } else {
+        btnLoadMore.style.display = 'none';
+    }
+}
 
+const btnLoadMore = document.querySelector('.amazingWork__btn');
+btnLoadMore.addEventListener('click', (e) => {
+    const dataTab = document.querySelector('.amazingWork__title.active').getAttribute('data-tab');
 
-
-
-
-
-
-
-
-
-
-
+    currentCountCategoryItems += showCards;
+    renderAmazingWorkCards(dataTab);
+})
 
 
 // -----------------------------8 секція----Gallery of best images-----------------------------
-$(document).ready(function(){
+$(document).ready(function () {
     $('#gallery__container').masonry({
 // указываем элемент-контейнер в котором расположены блоки для динамической верстки
         itemSelector: '.item',
